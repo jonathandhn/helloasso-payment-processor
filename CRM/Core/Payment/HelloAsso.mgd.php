fef6c9fc0910878c5d8660be2c07759737606f79
Dewy <dewymercerais@gmail.com>
2024-07-23 14:57:31 +0200

    AB: PAsser le payment processor type en active dans le fichier managed

--- CRM/Core/Payment/HelloAsso.mgd.php
+++ CRM/Core/Payment/HelloAsso.mgd.php
@@ -10,7 +10,8 @@ return [
             'name' => 'HelloAsso',
             'description' => 'HelloAsso Payments Processor',
             'class_name' => 'Payment_HelloAsso',
-
+            'is_active' => TRUE,
+            'is_default' => FALSE,
             'user_name_label' => 'Client Id',
             'password_label' => 'Client Secret',
             'subject_label' => 'Organization Name',
